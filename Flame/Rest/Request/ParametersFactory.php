a4b04e6a13eb63ae57e723482d5d73d952cc7b3b
JSifalda <sifalda.jiri@gmail.com>
2013-11-04 20:56:53 +0100

    Parse post data from form

--- Flame/Rest/Request/ParametersFactory.php
+++ Flame/Rest/Request/ParametersFactory.php
@@ -43,7 +43,42 @@ class ParametersFactory extends Object implements IParametersFactory
 			$default['data'] = $this->httpRequest->getPost();
 		}
 
+		if($default['format'] !== 'json') {
+			$default['data'] = $this->formatData($default['data']);
+		}
+
 		return $default;
+	}
+
+	/**
+	 * @param mixed $data
+	 * @return mixed
+	 */
+	private function formatData($data)
+	{
+		if(is_string($data) && strpos($data, '=') !== false) {
+			if(strpos($data, '&') !== false) {
+				$data = explode('&', $data);
+			}
+
+			if(!is_array($data)) {
+				$data = array($data);
+			}
+
+			if(count($data)) {
+				$newData = array();
+				foreach ($data as $item) {
+					$item = explode('=', $item);
+					if(isset($item[0], $item[1])) {
+						$newData[$item[0]] = $item[1];
+					}
+				}
+
+				$data = $newData;
+			}
+		}
+
+		return $data;
 	}
 
 } 
