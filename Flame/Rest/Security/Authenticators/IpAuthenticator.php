03721b1a84841158b2bcac07db256980431804ea
JSifalda <sifalda.jiri@gmail.com>
2014-02-15 10:29:56 +0100

    Imporve getting client ip

--- Flame/Rest/Security/Authenticators/IpAuthenticator.php
+++ Flame/Rest/Security/Authenticators/IpAuthenticator.php
@@ -40,8 +40,14 @@ class IpAuthenticator extends Object implements IAuthenticator
 	 */
 	protected function getClientIp()
 	{
-		if (isset($_SERVER['REMOTE_ADDR'])) {
-			return $_SERVER['REMOTE_ADDR'];
+		foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
+			if (array_key_exists($key, $_SERVER) === true) {
+				foreach (explode(',', $_SERVER[$key]) as $ip) {
+					if (filter_var($ip, FILTER_VALIDATE_IP) !== false) {
+						return $ip;
+					}
+				}
+			}
 		}
 	}
 }
